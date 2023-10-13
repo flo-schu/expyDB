111031f73e319b29ce7a95545dd0d2cad73f78a4
flo-schu <fluncki@protonmail.com>
2023-10-13 23:21:07 +0200

    set up project

---
+++ experiment-db/__init__.py
@@ -0,0 +1,13 @@
+from database_add_data import (
+    add_data,
+    remove_latest,
+    create_database,
+    delete_tables
+)
+
+from database_model import (
+    Experiment,
+    Treatment,
+    Observation
+)
+
