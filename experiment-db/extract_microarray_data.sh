83ae96a1b23beae60d7744de2f9858105baa479a
flo-schu <fluncki@protonmail.com>
2023-10-13 23:08:07 +0200

    remove old files from damage-proxy

--- experiment-db/extract_microarray_data.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-output="data/processed_data/ds1_microarray_data"
-mkdir $output
-tar -xvf data/dataset_1/GSE109496_RAW.tar -C $output
-
-for file in $output/*.gz; do
-    gzip -d "$file"
-done
