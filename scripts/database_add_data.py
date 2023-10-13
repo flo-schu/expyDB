c590da3343f507e2e9e8c19dd2ea56e931604431
flo-schu <fluncki@protonmail.com>
2023-10-13 22:34:23 +0200

    Refactor data addition

--- scripts/database_add_data.py
+++ scripts/database_add_data.py
@@ -1,9 +1,11 @@
 import warnings
 import os
+from typing import List
 import pandas as pd
 import numpy as np
+import datetime
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 from database_model import Observation, Treatment, Experiment, Base
 from timepath.helpers.misc import label_duplicates, replace_na
 
@@ -45,81 +47,139 @@ data["unit"] = data.measurement.map(measurement_variables)
 data = replace_na(data, "experiment_date", default_value=pd.to_datetime("1900-01-01"))
 data = replace_na(data, "experimentator", default_value="UNKNOWN")
 
+experiment_variables = ["experiment_date", "experimentator"]
 treatment_vars = ["substance", "mix", "cext_nom", "cext_nom_total", "hpf", "n_zfe"]
 
-nans = data[treatment_vars].isna().values.sum(axis=0)
+def treatment_setter(variables, identifiers):
+    (substance, mix, cext_nom, cext_nom_tot, hpf, n_zfe) = identifiers
+                    
+    if not mix:
+        exposure_map = {f"cext_nom_{substance.lower()}": cext_nom}
+    else:
+        # this is in information about the mixture that I got from 
+        # 2018 and 2019 experiments
+        exposure_map = {
+            "cext_nom_diuron": cext_nom_tot * 0.11,
+            "cext_nom_diclofenac": cext_nom_tot * 1000 * 0.026,
+            "cext_nom_naproxen": cext_nom_tot * 1000 * 0.864,
+        }
 
-if np.any(nans > 0):
-    warnings.warn(
-        f"NaNs in treatment variables {np.array(treatment_vars)[nans > 0]} detected. " 
-        "Fix in data input, define default, or live with nans in treatment info."
+    return dict(
+        hpf=hpf,
+        nzfe=n_zfe,
+        **exposure_map,
     )
 
-# Create an engine to connect to your database
-# SQLAlchemy does not suppor the addition of columns. This has to be done
-# by hand, but this is also not such a big deal. 
-database = "data/tox.db"
+def experiment_setter(variables, identifiers):
+    experiment_date, experimentator = identifiers
+    return {"date": experiment_date, "experimentator": experimentator}
 
-engine = create_engine(f"sqlite:///{database}", echo=False)
-session = Session(engine)
-if os.path.exists(database):
-    Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+def setter(variables, identifiers):
+    return {key:value for key, value in zip(variables, identifiers)}
 
+def add_data(
+    database: str,
+    data: pd.DataFrame,
+    experiment_variables: List[str],
+    treatment_variables: List[str],
+    experiment_setter: callable = setter,
+    treatment_setter: callable = setter,
+):
 
-
-with Session(engine) as session:
-    # group by experiment
-    exp_groups = data.groupby(["experiment_date", "experimentator"], dropna=False)
-    for (exp_date, experimentator), experiment_rows in exp_groups:
-        experiment = Experiment(
-            date=exp_date,
-            experimentator=experimentator
+    nans = data[treatment_vars].isna().values.sum(axis=0)
+    if np.any(nans > 0):
+        warnings.warn(
+            f"NaNs in treatment variables {np.array(treatment_vars)[nans > 0]} detected. " 
+            "Fix in data input, define default, or live with nans in treatment info."
         )
-        treat_groups = experiment_rows.groupby(treatment_vars,dropna=False)
 
-        # group experiments by treatments
-        for treatment_vals, treatment_rows in treat_groups:
-            # TODO: Future this could be modified so that a treatment specific
-            # function is provided to modify the information, returning a dict
-            # with the kwargs of Treatment(**kwargs)
-            
-            (substance, mix, cext_nom, cext_nom_tot, hpf, n_zfe) = treatment_vals
-            
-            if not mix:
-                exposure_map = {f"cext_nom_{substance.lower()}": cext_nom}
-            else:
-                # this is in information about the mixture that I got from 
-                # 2018 and 2019 experiments
-                exposure_map = {
-                    "cext_nom_diuron": cext_nom_tot * 0.11,
-                    "cext_nom_diclofenac": cext_nom_tot * 1000 * 0.026,
-                    "cext_nom_naproxen": cext_nom_tot * 1000 * 0.864,
-                }
-            # print(n_zfe)
-            treatment = Treatment(
-                experiment=experiment,
-                hpf=hpf,
-                nzfe=n_zfe,
-                **exposure_map,
+    # Create an engine to connect to your database
+    CREATED_AT = datetime.datetime.now()
+    engine = create_engine(f"sqlite:///{database}", echo=False)
+
+    with Session(engine) as session:
+
+        # group by experiment
+        exp_groups = data.groupby(experiment_variables, dropna=False)
+        for experiment_identifiers, experiment_rows in exp_groups:
+            experiment = Experiment(
+                created_at=CREATED_AT,
+                **experiment_setter(experiment_variables, experiment_identifiers)
             )
 
-            # iterate over measurement variables (when data are organized not in
-            # long format)
-            label_duplicates(treatment_rows, index=["time"])
-            # iterate over observations in treatment
-            for key, row in treatment_rows.iterrows():
-                observation = Observation(
+            # group experiments by treatments
+            treat_groups = experiment_rows.groupby(treatment_variables, dropna=False)
+            for treatment_identifiers, treatment_rows in treat_groups:
+                treatment = Treatment(
+                    created_at=CREATED_AT,
                     experiment=experiment,
-                    treatment=treatment,
-                    measurement=row.measurement,
-                    unit=row.unit,
-                    replicate_id=row.rep_no,
-                    time=row.time,
-                    value=row.value  
+                    **treatment_setter(treatment_variables, treatment_identifiers)
                 )
 
-                session.add(observation)
+                # assign duplicate keys for repeated measurements
+                if "replicate_id" not in treatment_rows.columns:
+                    label_duplicates(treatment_rows, index=["time", "measurement"])
+                
+                # iterate over observations in treatment
+                for _, row in treatment_rows.iterrows():
+                    observation = Observation(
+                        created_at=CREATED_AT,
+                        experiment=experiment,
+                        treatment=treatment,
+                        measurement=row.measurement,
+                        unit=row.unit,
+                        replicate_id=row.replicate_id,
+                        time=row.time,
+                        value=row.value  
+                    )
 
-    session.flush()
-    session.commit()
+                    session.add(observation)
+
+        session.flush()
+        session.commit()
+
+
+def remove_latest(database):
+    engine = create_engine(f"sqlite:///{database}", echo=False)
+    
+    with Session(engine) as session:
+        experiments = pd.read_sql(
+            select(Experiment), 
+            con=f"sqlite:///{database}"
+        )
+
+        created_last = experiments.created_at.unique()[-1]
+        stmt = select(Experiment).where(Experiment.created_at == created_last)
+
+        for row in session.execute(stmt):
+            session.delete(row.Experiment)
+
+        session.flush()
+        session.commit()
+
+def create_database(database):
+    # SQLAlchemy does not suppor the addition of columns. This has to be done
+    # by hand, but this is also not such a big deal. 
+    engine = create_engine(f"sqlite:///{database}", echo=False)
+    session = Session(engine)
+    if not os.path.exists(database):
+        Base.metadata.create_all(engine)
+
+def delete_tables(database):
+    engine = create_engine(f"sqlite:///{database}", echo=False)
+    session = Session(engine)
+    if os.path.exists(database):
+        Base.metadata.drop_all(engine)
+
+
+if __name__ == "__main__":
+    database = "data/tox.db"
+    create_database(database)
+    add_data(
+        database=database, 
+        data=data, 
+        experiment_variables=experiment_variables,
+        treatment_variables=treatment_vars,
+        experiment_setter=experiment_setter,
+        treatment_setter=treatment_setter,
+    )
